c8c3115216963147b5a4c1fdcf9dcacf4b7ea8c5
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-01-03 18:47:22 +0100

    some simplifications

--- braids.jl
+++ braids.jl
@@ -1,33 +1,32 @@
-using Compose, Colors
+using Compose: stroke, compose, context, linewidth, text, font
+using Colors
 
-struct Braid{N}
+
+struct Braid
     els::Vector{Int} 
-    function Braid{N}(x::Vector{Int}) where N
-        @assert all(1 ≤ abs(v) ≤ N - 1 for v in x)
-        new{N}(x)
-    end
 end
 
-Braid{N}(x::Int) where N = Braid{N}([x])
+braid(x...) = Braid(Int[x...])
 
-one(::Braid{N}) where N = Braid{N}(Int[])
+one(::Braid) = braid()
 
-function Base.:^(a::Braid{N}, k::Integer) where N
+function Base.:^(a::Braid, k::Integer)
     if k < 0
-        Braid{N}(reduce(vcat, fill(reverse(.-a.els), abs(k))))
+        Braid(reduce(vcat, fill(reverse(.-a.els), abs(k))))
     elseif k > 0
-        Braid{N}(reduce(vcat, fill(a.els, k)))
+        Braid(reduce(vcat, fill(a.els, k)))
     else
-        Braid{N}(Int[])
+        Braid(Int[])
     end 
 end
 
-Base.:*(a::Braid{N}, b::Braid{N}) where N = Braid{N}(vcat(a.els,b.els))
+Base.:*(a::Braid, b::Braid) = Braid(vcat(a.els,b.els))
 
-Base.inv(a::Braid{N}) where N = Braid{N}(reverse(.- a.els))
+Base.inv(a::Braid) = Braid(reverse(.- a.els))
 
+nstrands(a::Braid) = maximum(abs, a.els; init=0) + 1
 
-function plot(a::Braid{N}; cols=HSV.((0:N-1)./(N-1)*256,1,1), bcol="black") where N
+function plot(a::Braid; N = nstrands(a), cols=HSV.((0:N-1)./(N-1)*256,1,1), bcol="black")
     cols = copy(cols)
     Δt = 3mm
     set_default_graphic_size(Δt * (length(a.els) + 1), N*2mm)
@@ -60,7 +59,7 @@ function plot(a::Braid{N}; cols=HSV.((0:N-1)./(N-1)*256,1,1), bcol="black") wher
     c = compose(context(0,0.1,1,0.8), strings...)
 end
 
-function _plot(v::AbstractVector{Int}, N::Int, cols, bcol)
+function _plot(v::AbstractVector{Int}, N, cols, bcol)
     Δt = 3mm
     isempty(v) && return context()
     l1 = curve((0,0), (0.5,0), (0.5,1),(1,1))
@@ -83,57 +82,63 @@ function _plot(v::AbstractVector{Int}, N::Int, cols, bcol)
     compose(c1, compose(context(Δt,0,Δt*(length(v)-1),1), c2))
 end
 
-function plot2(a::Braid{N}; cols=HSV.((0:256/(N-1):256),1,1), bcol="black") where N
+function plot2(a::Braid; N = nstrands(a), cols=HSV.((0:256/(N-1):256),1,1), bcol="black")
     Δt = 3mm
     set_default_graphic_size(Δt * (length(a.els) + 1), N*2mm)
     cols = copy(cols)
-    compose(context(0,0.1,1,0.8), _plot(view(a.els,:), N, cols, bcol))
+    compose(context(0,0.1,1,0.8), _plot(a.els, N, cols, bcol))
 end
 
-function Base.show(io::IO, mime::MIME"text/html", a::Braid{N}) where N
+function Base.show(io::IO, mime::MIME"text/html", a::Braid)
+    N = nstrands(a)
     cols = get(io, :cols, HSV.(0:256/(N-1):256,1,1))
     bcol = get(io, :bcol, "black")
-    show(io, mime, plot(a; cols, bcol))
+    show(io, mime, plot(a; N, cols, bcol))
+end
+
+function freesimplify!(a::Braid)
+    e = a.els
+    isempty(e) && return a
+    i = firstindex(e)
+    j = i+1
+    
+    while j <= length(e)
+        if a[i] + a[j] == 0
+            a[i] = a[j] = 0
+            i > 1 && (i -= 1)
+        else
+            i = j
+        end
+        j += 1
+    end
+end
+
+
+function inpowers(a::Braid)
+    w = Tuple{Int,Int}[]
+    for x in a.els
+        i, σ = abs(x), sign(x)
+        if !isempty(w) && w[end][1] == i
+            if w[end][2]+σ == 0
+                pop!(w)
+            else
+                w[end] = (w[end][1],w[end][2]+σ)
+            end
+        else
+            push!(w, (i,σ))
+        end
+    end
+    w
 end
 
 function Base.show(io::IO, a::Braid)
-    v = if get(io, :compact, false)
-        w = Tuple{Int,Int}[]
-        for x in a.els
-            i,σ = abs(x), sign(x)
-            if !isempty(w) && w[end][1] == i
-                if w[end][2]+σ == 0
-                    pop!(w)
-                else
-                    w[end] = (w[end][1],w[end][2]+σ)
-                end
-            else
-                push!(w, (i,σ))
-            end
-        end
-        w
-    else
-        [(abs(x), sign(x)) for x in a.els]
-    end
-
-    print(io, prod("σ".*subscripts.(first.(v)).*superscripts.(last.(v))))
+    isempty(a.els) && print(io, "ε")
+    v = get(io, :compact, false) ? inpowers(a) : [(abs(x), sign(x)) for x in a.els]
+    print(io, prod("σ"*subscripts(i).*superscripts(k) for (i,k) in v; init=""))
 end
 
 
+superscripts(x) = x == 1 ? "" : replace(string(x), (a=>b for (a,b) in zip("-01234567890","⁻⁰¹²³⁴⁵⁶⁷⁸⁹"))...)
+subscripts(x) = replace(string(x), (a=>b for (a,b) in zip("-01234567890","₋₀₁₂₃₄₅₆₇₈₉"))...)
 
-function superscripts(x)
-    x == 1 && return ""
-    x, σ = abs(x), sign(x)
-    s = prod(getindex.((("⁰","¹","²","³","⁴","⁵","⁶","⁷","⁸","⁹"),), 
-                reverse(digits(x)).+1))
-    σ == 1 ? s : "⁻" * s
-end 
-
-function subscripts(x)
-    x, σ = abs(x), sign(x)
-    s = prod(getindex.((("₀","₁","₂","₃","₄","₅","₆","₇","₈","₉"),), 
-                reverse(digits(x)).+1))
-    sign(x) == 1 ? s : "₋" * s 
-end 
-
- nothing
+nothing
