6be183cc4c3fff743bfb340e5c4344db6f189e41
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-01-04 02:48:26 +0100

    more fixes

--- braids.jl
+++ braids.jl
@@ -4,7 +4,7 @@ using Colors
 
 struct Braid
     els::Vector{Int}
-    function Braid(v::AbstractVector{<:Integer})
+    function Braid(v::Vector{Int})
         @assert all(!iszero, v)
         new(v)
     end
@@ -41,8 +41,8 @@ function plot(a::Braid; N = nstrands(a), cols=HSV.((0:N-1)./(N-1)*256,1,1), bcol
     l2 = curve((0,1), (0.5,1), (0.5,0),(1,0))
     
     f(l; col="white", bcol="black") = compose(context(), 
-        compose(context(), l, stroke(col), linewidth(0.2mm)),
-        compose(context(), l, stroke(bcol), linewidth(1mm)) 
+        (context(), l, stroke(col), linewidth(0.2mm)),
+        (context(), l, stroke(bcol), linewidth(1mm)) 
     )
     
     function drawsigma!(i,n;cols=fill("white",n), bcol="black")
@@ -50,9 +50,9 @@ function plot(a::Braid; N = nstrands(a), cols=HSV.((0:N-1)./(N-1)*256,1,1), bcol
         myline(j) = compose(context(), line([(0,(j-1)/(n-1)),(1,(j-1)/(n-1))]), stroke(cols[j]))
         myline2(j) = line([(0,(j-1)/(n-1)),(1,(j-1)/(n-1))])
         c = compose(context(),
-            compose(context(), myline.([j for j=1:n if j ∉ (i, i + 1)])..., linewidth(0.2mm)), 
-            compose(context(), myline2.([j for j=1:n if j ∉ (i, i + 1)])..., stroke(bcol), linewidth(1mm)), 
-            compose(context(0,(i-1)/(n-1),1,1/(n-1)), 
+                (context(), myline.([j for j=1:n if j ∉ (i, i + 1)])..., linewidth(0.2mm)), 
+                (context(), myline2.([j for j=1:n if j ∉ (i, i + 1)])..., stroke(bcol), linewidth(1mm)), 
+                (context(0,(i-1)/(n-1),1,1/(n-1)), 
                     σ == 1 ? 
                     compose(context(), f(l1; col=cols[i]), f(l2; col=cols[i+1], bcol)) : 
                     compose(context(), f(l2; col=cols[i+1]), f(l1; col=cols[i], bcol))))
@@ -60,7 +60,7 @@ function plot(a::Braid; N = nstrands(a), cols=HSV.((0:N-1)./(N-1)*256,1,1), bcol
         c
     end
 
-    T = length(a.els)
+    T = length(a)
     strings = [compose(context((k-1)*Δt,0,Δt,1),drawsigma!(a.els[k],N;cols,bcol)) for k=1:T]
     c = compose(context(0,0.1,1,0.8), strings...)
 end
@@ -102,7 +102,7 @@ function Base.show(io::IO, mime::MIME"text/html", a::Braid)
     show(io, mime, plot(a; N, cols, bcol))
 end
 
-"Do a free simplificaton of a, cancelling out all products of inverses"
+"Do an in-place free simplificaton of a, cancelling out all products of inverses"
 function freesimplify!(a::Braid)
     e = a.els
     length(e) <= 1 && return a
@@ -129,7 +129,9 @@ function ishandle(a::Braid, i::Int, j::Int)
     return all(abs(a.els[k]) ∉ (x-1, x) for k in i+1:j-1) 
 end
 
-
+"""
+Find next permitted handle of `a`, return `0,0`` if `a` is already reduced 
+"""
 function nexthandle(a::Braid)
     for j = 2:length(a.els)
         for i = j-1:-1:1
@@ -139,6 +141,9 @@ function nexthandle(a::Braid)
     return 0,0
 end
 
+"""
+One H-step of Dehornoy reduction of `a` on handle `i,j`
+"""
 function reduced(a::Braid, i::Int, j::Int)
     xi,si = abs(a.els[i]), sign(a.els[i])
     @assert (xi,si) == (abs(a.els[j]),-sign(a.els[j]))
@@ -158,6 +163,9 @@ function reduced(a::Braid, i::Int, j::Int)
     freesimplify!(Braid(H))
 end
 
+"""
+Dehornoy reduction of `a`
+"""
 function reduced(a::Braid)
     a = freesimplify!(Braid(copy(a.els)))
     while true
@@ -179,6 +187,10 @@ function isreduced(a::Braid)
     maingen = main_generator(a)
     return all(!=(maingen), a.els) || all(!=(-maingen), a.els)
 end
+
+Base.:(==)(a::Braid, b::Braid) = isempty(reduced(a*b^-1).els)
+
+Base.length(a::Braid) = length(a.els)
 
 "Produce an expression of `a` in powers of generators, in the form of a Vector of tuples (generator, power)"
 function powers(a::Braid)
@@ -203,7 +215,6 @@ function Base.show(io::IO, a::Braid)
     v = get(io, :compact, false) ? powers(a) : [(abs(x), sign(x)) for x in a.els]
     print(io, prod("σ"*subscripts(i)*superscripts(k) for (i,k) in v; init=""))
 end
-
 
 superscripts(x) = x == 1 ? "" : replace(string(x), (a=>b for (a,b) in zip("-01234567890","⁻⁰¹²³⁴⁵⁶⁷⁸⁹"))...)
 subscripts(x) = replace(string(x), (a=>b for (a,b) in zip("-01234567890","₋₀₁₂₃₄₅₆₇₈₉"))...)
