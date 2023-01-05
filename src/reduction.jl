dcaa7e2ef6173048239a510dac5063901f6082c0
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-01-05 15:39:55 +0100

    reduction.jl cleanup

--- src/reduction.jl
+++ src/reduction.jl
@@ -1,4 +1,4 @@
-"Do an in-place free simplificaton of a, cancelling out all products of inverses"
+"Do an in-place free simplificaton of a, cancelling out consecutive inverses"
 function freesimplify!(a::Braid)
     length(a) <= 1 && return a
     
@@ -16,6 +16,69 @@ function freesimplify!(a::Braid)
     return a
 end
 
+"Find next *permitted* handle of `a`, return `0,0`` if `a` is already reduced"
+function nexthandle(a::Braid, last = fill(0, width(a)))
+    length(a) < 2 && return 0,0
+    last .= 0
+    for j = 1:length(a)
+        xj,sj = a[j]
+        i = last[xj] # i is a candidate
+        if i >= 1 && sj != sign(a.els[i]) && (xj == 1 || last[xj - 1] < i)
+            return i,j
+        end
+        last[xj] = j
+    end
+    return 0,0
+end
+
+"In-place Dehornoy H-step"
+function reduced!(a::Braid, i::Int, j::Int)
+    (xi,si), (xj,sj) = a[i], a[j]
+    @assert (xi,si) == (xj,-sj)
+    oldb1, oldb2 = i+1:j-1, j+1:length(a)
+    numk = count(x->abs(x) == xi + 1, @view a.els[oldb1])
+    newb1, newb2, newb = oldb1 .+ (2numk - 1), oldb2 .+ (2numk - 2), i:j+2numk-2
+    if numk > 0
+        resize!(a.els, length(a) + 2numk - 2)
+        a.els[newb2] = @view a.els[oldb2]
+        a.els[newb1] = @view a.els[oldb1]
+        replace((xk, sk)) = xk == xi + 1 ? (xk*sj, xi*sk, xk*si) : xk*sk
+        a.els[newb] .= Iterators.flatten(replace(a[k]) for k in newb1)
+    else # just remove i and j
+        a.els[newb1] = @view a.els[oldb1]
+        a.els[newb2] = @view a.els[oldb2]
+        resize!(a.els, length(a) - 2)
+    end
+    freesimplify!(a)
+end
+
+"Dehornoy reduction of `a`"
+function reduced!(a::Braid)
+    last = fill(0, width(a))
+    freesimplify!(a)
+    while ((i, j) = nexthandle(a, last)) != (0, 0)
+        reduced!(a, i, j)
+    end
+    a
+end
+
+reduced(a::Braid) = reduced!(copy(a))
+
+"Braid group equivalence, i.e. true if `a` and `b` represent the same group element"
+Base.:(==)(a::Braid, b::Braid) = isone(reduced!(inv(a)*b))
+
+"Fetch the main generator of `a`"
+main_generator(a::Braid) = isone(a) ? 0 : argmin(abs, a.els)
+
+"Braid comparison"
+Base.:(<)(a::Braid, b::Braid) = main_generator(reduced!(inv(a)*b)) > 0
+
+Base.isless(a::Braid, b::Braid) = a < b
+
+
+### slow reduction methods, useful for debugging
+###
+
 "Is `i, j` a handle for `a`?"
 function ishandle(a::Braid, i::Int, j::Int)
     a.els[i] + a.els[j] != 0 && return false
@@ -28,22 +91,6 @@ function nexthandle_slow(a::Braid)
         for i = j-1:-1:1
             ishandle(a, i, j) && return i,j       
         end
-    end
-    return 0,0
-end
-
-
-"Find next *permitted* handle of `a`, return `0,0`` if `a` is already reduced"
-function nexthandle(a::Braid, last = fill(0, width(a)))
-    length(a) < 2 && return 0,0
-    last .= 0
-    for j = 1:length(a)
-        xj,sj = a[j]
-        i = last[xj] # i is a candidate
-        if i >= 1 && sj != sign(a.els[i]) && (xj == 1 || last[xj - 1] < i)
-            return i,j
-        end
-        last[xj] = j
     end
     return 0,0
 end
@@ -68,38 +115,6 @@ function reduced_slow(a::Braid, i::Int, j::Int)
     freesimplify!(Braid(H))
 end
 
-"In-place Dehornoy H-step"
-function reduced!(a::Braid, i::Int, j::Int)
-    (xi,si), (xj,sj) = a[i], a[j]
-    @assert (xi,si) == (xj,-sj)
-    numk = count(x->abs(x) == xi + 1, @view a.els[i+1:j-1])
-    oldb1, oldb2 = i+1:j-1, j+1:length(a)
-    newb1, newb2, newb = oldb1 .+ (2numk - 1), oldb2 .+ (2numk - 2), i:j+2numk-2
-    if numk > 0
-        resize!(a.els, length(a) + 2numk - 2)
-        a.els[newb2] = @view a.els[oldb2]
-        a.els[newb1] = @view a.els[oldb1]
-        replace((xk, sk)) = xk == xi + 1 ? (xk*sj, xi*sk, xk*si) : xk*sk
-        a.els[newb] .= Iterators.flatten(replace(a[k]) for k in newb1)
-    else # numk == 0, just remove i and j
-        a.els[newb1] = @view a.els[oldb1]
-        a.els[newb2] = @view a.els[oldb2]
-        resize!(a.els, length(a) - 2)
-    end
-    freesimplify!(a)
-end
-
-
-"Dehornoy reduction of `a`"
-function reduced!(a::Braid)
-    last = fill(0, width(a))
-    freesimplify!(a)
-    while ((i, j) = nexthandle(a, last)) != (0, 0)
-        reduced!(a, i, j)
-    end
-    a
-end
-
 function reduced_slow(a::Braid)
     a = freesimplify!(copy(a))
     while ((i, j) = nexthandle_slow(a)) != (0, 0)
@@ -107,26 +122,3 @@ function reduced_slow(a::Braid)
     end
     a
 end
-
-reduced(a::Braid) = reduced!(copy(a))
-
-"Fetch the main generator of `a`"
-main_generator(a::Braid) = isone(a) ? 0 : argmin(abs, a.els)
-
-"Is `i,j` a main handle for `a`?"
-ismainhandle(a::Braid, i::Int, j::Int) = ishandle(a, i, j) && abs(a.els[i]) == abs(main_generator(a))
-
-"Is `a` reduced?"
-function isreduced(a::Braid)
-    isempty(a.els) && return true
-    maingen = main_generator(a)
-    return all(!=(maingen), a.els) || all(!=(-maingen), a.els)
-end
-
-"Braid equivalence, are `a` and `b` the same group element?"
-Base.:(==)(a::Braid, b::Braid) = isone(reduced!(inv(a)*b))
-
-"Braid comparison"
-Base.:(<)(a::Braid, b::Braid) = main_generator(reduced!(inv(a)*b)) > 0
-
-Base.isless(a::Braid, b::Braid) = a < b
