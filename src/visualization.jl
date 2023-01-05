61a8d85b67ebd313392da50ac339e9bd2dc1d874
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-01-05 13:35:34 +0100

    cleaner code

--- src/visualization.jl
+++ src/visualization.jl
@@ -5,26 +5,25 @@ function composed(a::Braid; compressed=true, Δt = 3mm, Δy = 2mm, cols=nothing,
     T,N = length(a), width(a)
     cols = isnothing(cols) ? HSV.(StepRangeLen(0,360/N,N),1,1) : copy(cols)
     set_default_graphic_size(Δt * max(T, 1), Δy * N)
-
-    l1 = curve((0,0), (0.5,0), (0.5,1),(1,1))
-    l2 = curve((0,1), (0.5,1), (0.5,0),(1,0))
     
-    f(l, col) = compose(context(), 
+    cline(l, col) = compose(context(), 
         (context(), l, stroke(col), linewidth(0.2mm)),
         (context(), l, stroke(bcol), linewidth(1mm)))
 
     pos(j) = (j-0.5)/N
 
-    crossing(x, s, n) = compose(context(Δt*n, pos(x), Δt, 1/N), 
-            s == 1 ? 
-            (context(), f(l1, cols[x]), f(l2, cols[x+1])) : 
-            (context(), f(l2, cols[x+1]), f(l1, cols[x])))
+    function crossing(x, s, n)
+        con = context(Δt*n, pos(x), Δt, 1/N)
+        c1 = cline(curve((0,0), (0.5,0), (0.5,1),(1,1)), cols[x])
+        c2 = cline(curve((0,1), (0.5,1), (0.5,0),(1,0)), cols[x+1])
+        s == 1 ? compose(con, c1, c2) : compose(con, c2, c1)
+    end
 
     hline(x, n1, n2) = compose(context(), 
         (context(), line([(Δt*n1,pos(x)), (Δt*n2,pos(x))]), stroke(cols[x]), linewidth(0.2mm)),
         (context(), line([(Δt*n1,pos(x)), (Δt*n2,pos(x))]), stroke(bcol), linewidth(1mm)))
 
-    c = compose(context(0,0,1,1))
+    c = context()
 
     T == 0 && return compose(c, hline(1, 0, 1))
     
