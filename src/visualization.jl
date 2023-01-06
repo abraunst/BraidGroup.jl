19ca395c6ae256c8cde0f7a5cc3a0a092a58e898
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-01-06 16:53:02 +0100

    better visualization

--- src/visualization.jl
+++ src/visualization.jl
@@ -4,12 +4,11 @@ using Compose, Colors
 function composed(a::Braid; compressed=true, Δt = 3mm, Δy = 2mm, cols=nothing, bcol="black")
     T,N = length(a), width(a)
     cols = isnothing(cols) ? HSV.(StepRangeLen(0,360/N,N),1,1) : copy(cols)
-    set_default_graphic_size(Δt * max(T, 1), Δy * N)
 
     # dropped shadow
     shadow(l, col) = compose(context(), 
         (context(), l, stroke(col), linewidth(Δy/10)),
-        (context(), l, stroke(bcol), linewidth(Δy/2)))
+        bcol == nothing ? context() : (context(), l, stroke(bcol), linewidth(Δy/2)))
 
     function crossing(x, s, t)
         con = context(Δt*t, (x-0.5)/N, Δt, 1/N)
@@ -37,11 +36,10 @@ function composed(a::Braid; compressed=true, Δt = 3mm, Δy = 2mm, cols=nothing,
     for x = 1:N
         c = compose(c, hline(x, lastx[x],  endx))
     end
-    return c
-end
 
-function Base.show(io::IO, mime::MIME"text/html", a::Braid)
-    show(io, mime, composed(a, compressed=false))
+    set_default_graphic_size(Δt * (endx + 2), Δy * N)
+
+    return c
 end
 
 "Produce an expression of `a` in powers of generators, in the form of a Vector of tuples (generator, power)"
@@ -82,4 +80,12 @@ function Base.show(io::IO, ::MIME"text/latex", a::Braid)
     end
     isempty(p) && print(io, "\\varepsilon")
     print(io, "\$")
+end
+
+function Base.show(io::IO, mime::MIME"text/html", a::Braid)
+    show(io, mime, composed(a, compressed=false))
+end
+
+function Base.show(io::IO, mime::MIME"image/svg+xml", a::Braid)
+    show(io, mime, composed(a, compressed=false))
 end
