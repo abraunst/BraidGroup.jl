d7368422a9d5be6ce07cb72a8d6f2c8ded4b71ea
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-03-20 11:02:18 +0100

    name fix

--- src/BraidGroup.jl
+++ src/BraidGroup.jl
@@ -1,6 +1,6 @@
 module BraidGroup
 
-export Braid, braid, reduced, reduced!, freesimiplify!, width, composed, 
+export Braid, braid, reduced, reduced!, freesimplify!, width, composed, 
     garside_conjugate!, garside_conjugate, randbraid, compress, permutation
 
 include("braid.jl")
