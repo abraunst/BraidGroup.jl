d4eb5371c3411e3d183e48a3c46e80afedf7884e
Alfredo Braunstein <alfredo.braunstein@polito.it>
2025-02-17 08:26:13 +0100

    add plot, better compose

--- src/BraidGroup.jl
+++ src/BraidGroup.jl
@@ -1,7 +1,7 @@
 module BraidGroup
 
 export Braid, braid, reduced, reduced!, freesimplify!, width, composed, 
-    garside_conjugate!, garside_conjugate, randbraid, compress, permutation
+    garside_conjugate!, garside_conjugate, randbraid, compress, permutation, plot
 
 include("braid.jl")
 include("reduction.jl")
